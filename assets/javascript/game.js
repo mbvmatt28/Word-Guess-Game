6bcfd957407afb02e46295c38eb0b748cd0d379b
Matt Vanley <matt@vibrantsitedesign.com>
2019-03-07 21:14:22 -0600

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,17 @@
 var words = ["Jack Sparrow", "Eye Patch", "Parrot", "Treasure", "Captain Hook", "Pirate Ship", "Buccaneer"]
 
-var chosenWord = math.floor(math.random() * words.length);
-console.log(chosenWord)
 
-function words() {
-    var randomword = math.floor(math.random()*(words.length));
-    document.getElementById("message").innerHTML = words[randomword];
+document.getElementById("words").onclick = function() {
+    
+    var randomWord = Math.floor(Math.random()*words.length);
+    document.getElementById("message").innerHTML = words[randomWord];
+    console.log(randomWord);
+    document.getElementById("message")
 }
+
+
+
+
+    
+
+
