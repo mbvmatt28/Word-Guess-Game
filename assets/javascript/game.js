fd2feeee7a9a08e899a80c1427fc132205a44d03
Matthew Vanley <matt@vibrantsitedesign.com>
2019-03-09 23:44:01 -0600

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,71 @@
 //Array of Pirate Words
-var words = ["Captain", "Plank", "Parrot", "Treasure", "Hook", "Ship", "Buccaneer"]
+var words = ["captain", "plank", "parrot", "treasure", "hook", "ship", "buccaneer"];
 
-//click to generate new word
-document.getElementById("words").onclick = function() {
-    
-    var randomWord = Math.floor(Math.random()*words.length);
-    document.getElementById("message").innerHTML = words[randomWord];
-    console.log(randomWord);
-    document.getElementById("message")
+//generate new word
 
     
- //variables   
-    rightWord = [];
-    wrongWord = [];
+    
+    //document.getElementById("message").innerHTML = words[randomWord];
+   // document.getElementById("message");
+
+    
+ //variables 
+    var randomWord = Math.floor(Math.random()*words.length);  
+    rightLetter = []; 
+    wrongLetter = [];
     var underScore = [];
     var chosenWord = words[randomWord];
 
-    underScore[chosenWord.indexOf('keypress', (event) => {
-        docUnderScore[0] = document.getElementsByClassName('underscore');
-    })]
+//DOM Manipulation
+   // underScore[chosenWord.indexOf('keypress', (event) => {
+       
+        docUnderScore = document.getElementsByClassName('underscore');
+        docRightGuess = document.getElementsByClassName('rightGuess');
+        docWrongGuess = document.getElementsByClassName('wrongGuess');
+   // })]
     
 
 //Create Underscores based on length of word
     generateUnderscore = () => {
-        for(i = 0; i < chosenWord.length; i++) {
-            underScore.push('_')
+        for(let i = 0; i < chosenWord.length; i++) {
+            underScore.push('_');
         }
-        return underScore;       
+        return underScore;    
+        
     }
+    document.write(generateUnderscore);
     console.log(generateUnderscore());
 //Get User guess
-    document.addEventListener('keyup', (event) =>{
-        keycode = event.keyCode;
-        keyword = String.fromCharCode(keycode);
+    document.addEventListener('keypress', (event) => {
+        keyword = String.fromCharCode(event.keyCode);
 //If User guess is right     
         if(chosenWord.indexOf(keyword) > -1) {
-            rightWord.push(keyword);
-            docUnderScore.innerHTML = underScore;
+//Add to right letter array
+            rightLetter.push(keyword);
+            docUnderScore[0].innerHTML = underScore.join(' ');
+            docRightGuess[0].innerHTML = rightLetter;
+//Replace underscore with right letter
             underScore[chosenWord.indexOf(keyword)] = keyword;
+
+            
+           docRightGuess[0].innerHTML = rightLetter;
+            console.log(rightLetter);
+            
             if(underScore.join('') == chosenWord) {
                 alert('You Win!');
             }
         }
 //If User guess is wrong        
         else{
-            wrongWord.push(keyword);
+            wrongLetter.push(keyword);
+            console.log(wrongLetter);
+           docWrongGuess[0].innerHTML = wrongLetter;
+           
         }
+        //docUnderScore[0].innerHTML = generateUnderscore().join(' ');
     });
-    docunderScore[0].innerHTML = generateUnderscore().join(' ');
-}
+    
+
 
 
 
